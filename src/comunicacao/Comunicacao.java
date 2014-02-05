cd5947322a2a8f4ffcd2f5be4660a2afa1e89b77
Geba <gsp@cin.ufpe.br>
2014-02-05 19:44:29 -0300

    We Are Ready!

---
+++ src/comunicacao/Comunicacao.java
@@ -0,0 +1,23 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comunicacao;
+import gui.Principal;
+
+/**
+ *
+ * @author geeo
+ */
+public class Comunicacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal app = new Principal();
+        app.setVisible(true);
+    }
+}
