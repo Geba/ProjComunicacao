ecd31883b85599324350162c258c75e69e358bd9
Geba <gsp@cin.ufpe.br>
2014-02-05 20:14:28 -0300

    Criei Contato e Mensagem, no package atomics

---
+++ src/atomics/Mensagem.java
@@ -0,0 +1,22 @@
+package atomics;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author geeo
+ */
+public class Mensagem {
+
+    private String menssagem;
+    private String usuario;
+    private String hora;
+
+    public Mensagem() {
+
+    }
+
+}
