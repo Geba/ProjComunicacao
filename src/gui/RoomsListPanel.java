b5ad482653175633ccdfd36b7da0ff79f1c90968
Geba <gsp@cin.ufpe.br>
2014-03-09 01:18:19 -0300

    Gui ficando boa

--- src/gui/RoomsListPanel.java
+++ src/gui/RoomsListPanel.java
@@ -185,12 +185,11 @@ public class RoomsListPanel extends javax.swing.JPanel {
     }
 
     void showNewRoomWithoutOpen(Room room) {
-        RoomListItemPanel rlp = new RoomListItemPanel(room);
-        this.roomsItens.add(rlp);
-        rlp.setVisible(true);
-        this.roomsListPanelContainer.add(rlp);
-        this.roomsListPanelContainer.revalidate();;
-        System.out.println("0ioie");
+        RoomListItemPanel r1 = new RoomListItemPanel(room);
+                roomsItens.add(r1);
+                r1.setVisible(true);
+                this.listContainer.add(r1);
+                this.listContainer.revalidate();
         
         
     }
