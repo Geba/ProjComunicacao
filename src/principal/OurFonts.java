10a69b284e4281190f213b17c7eabba64939fe60
Geba <gsp@cin.ufpe.br>
2014-03-09 00:51:39 -0300

    Consertado o CoreServer

--- src/principal/OurFonts.java
+++ src/principal/OurFonts.java
@@ -1,4 +1,5 @@
 package principal;
+
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.io.FileInputStream;
@@ -8,30 +9,25 @@ import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class OurFonts {
 
-	
-public static Font getTitleFont(float tamanho) {
-	Font font = null;
-	try {
-		InputStream is = new FileInputStream("outros/fonts/texto.ttf");
+    public static Font getTitleFont(float tamanho) {
+        Font font = null;
+        try {
+            InputStream is = new FileInputStream("outros/fonts/texto.ttf");
             try {
-                font= Font.createFont(Font.TRUETYPE_FONT, is);
+                font = Font.createFont(Font.TRUETYPE_FONT, is);
             } catch (FontFormatException ex) {
                 Logger.getLogger(OurFonts.class.getName()).log(Level.SEVERE, null, ex);
             } catch (IOException ex) {
                 Logger.getLogger(OurFonts.class.getName()).log(Level.SEVERE, null, ex);
             }
-	
-	} catch (FileNotFoundException e) {
-		System.out.println("File not found");
-		e.printStackTrace();
-	}
-	return font.deriveFont(tamanho);
-}
-	
-	
-	
-	
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+        return font.deriveFont(tamanho);
+    }
+
 }
